2469f876eacd0e9650bce44f55a4352a00feba5d
Ekaterina <ketvolk@mail.ru>
2023-04-19 20:28:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 13676;
+        int bonusRub = 20;
+        int mile = cost / bonusRub;
+
+        System.out.println(mile);
+    }
+}
